e19618d1b432f2546c3a3f429c15330c44f0958e
Laila Campbell <lailacampbell96@gmail.com>
2020-03-15 19:29:56 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -4,6 +4,13 @@ class Waiter
   
   @@all = []
   
+  def initialize (name, years_experince)
+    @name = name
+    @years_experince = years_experince
+    @@all << self 
+  end 
+  
+  
   def self.all 
     @@all 
   end 
